e10b759c989b6372ea2660e89b12d6e52c9295cf
Andrew T. Baker <andrew.tork.baker@gmail.com>
2014-12-13 23:33:20 -0500

    Scaffoling out lobby

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -2,6 +2,15 @@
 
 angular.module('controllers', [])
 
-.controller('HomeCtrl', function($scope) {
+.controller('HomeCtrl', function($scope, Question) {
   $scope.foo = 'bar';
+  $scope.questions = Question.query();
+})
+
+.controller('GameCtrl', function($scope) {
+
+})
+
+.controller('LobbyCtrl', function($scope) {
+  $scope.players = ['foo', 'bar'];
 });
--- public/js/routes.js
+++ public/js/routes.js
@@ -5,11 +5,28 @@ angular.module('routes', ['ui.router'])
 .config(function($stateProvider, $urlRouterProvider, $locationProvider) {
   $stateProvider
     .state('home', {
-      url: "/",
+      url: '/',
       templateUrl: 'views/home.html',
       controller: 'HomeCtrl',
       data: {
         pageTitle: 'Home'
+      }
+    })
+
+    // setup an abstract state for the tabs directive
+    .state('game', {
+      url: '/game',
+      abstract: true,
+      template: '<ui-view/>',
+      controller: 'GameCtrl'
+    })
+
+    .state('game.lobby', {
+      url: '/lobby',
+      templateUrl: 'views/lobby.html',
+      controller: 'LobbyCtrl',
+      data: {
+        pageTitle: 'Lobby'
       }
     });
   
