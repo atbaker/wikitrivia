55bdda6a7919b11afaebe241e3b777b8cb4b861e
Andrew T. Baker <andrew.tork.baker@gmail.com>
2014-12-20 20:26:06 -0500

    New test data, 10 question game

--- app/models/question.js
+++ app/models/question.js
@@ -14,7 +14,7 @@ questionSchema.statics.generateQuestionSet = function(cb) {
   this.find({}, function(err, results) {
     var questions = [];
 
-    while (questions.length < 5) { // Change back to 10 after demo
+    while (questions.length < 10) { // Change back to 10 after demo
       var question = results[Math.floor(Math.random() * results.length)];
       var alreadyChosen = false;
       for (var i=0; i<questions.length; i++) {
