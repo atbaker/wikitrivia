8cf40c4dc9c08f34624b2ea8a8f2ffeaff1d06f4
Andrew T. Baker <andrew.tork.baker@gmail.com>
2015-03-27 18:05:47 -0400

    Trying to get New Relic working

--- newrelic.js
+++ newrelic.js
@@ -12,7 +12,7 @@ exports.config = {
   /**
    * Your New Relic license key.
    */
-  license_key : process.env.NEW_RELIC_LICENSE_KEY,
+  license_key : '',
   logging : {
     /**
      * Level at which to log. 'trace' is most useful to New Relic when diagnosing
